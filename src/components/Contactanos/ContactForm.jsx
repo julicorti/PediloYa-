45fa40af682cf3e48ce2f97613187d1233c76017
lospibes2 <thomas.babioet36@gmail.com>
2024-08-31 07:28:33 -0300

    Agr

--- src/components/Contactanos/ContactForm.jsx
+++ src/components/Contactanos/ContactForm.jsx
@@ -8,27 +8,26 @@ const ContactForm = () => {
           <p className="contact-form__subtitle">You can reach us anytime</p>
           <form className="contact-form__form">
             <div className="contact-form__row">
-              <input type="text" placeholder="First name" className="contact-form__input" />
-              <input type="text" placeholder="Last name" className="contact-form__input" />
+              <input type="text" placeholder="Nombre" className="contact-form__input" />
+              <input type="text" placeholder="Apellido" className="contact-form__input" />
             </div>
             <div className="contact-form__input-group">
               <span className="contact-form__icon">
                 <i className="fas fa-envelope"></i>
               </span>
-              <input type="email" placeholder="Your email" className="contact-form__input" />
+              <input type="email" placeholder="Email" className="contact-form__input" />
             </div>
             <div className="contact-form__phone-group">
               <select className="contact-form__select">
-                <option value="+62">+62</option>
+                <option value="+54">+54</option>
                 <option value="+1">+1</option>
                 <option value="+44">+44</option>
               </select>
-              <input type="text" placeholder="Phone number" className="contact-form__input contact-form__input--phone" />
+              <input type="text" placeholder="Numero de celular" className="contact-form__input contact-form__input--phone" />
             </div>
-            <textarea placeholder="How can we help?" maxLength="120" className="contact-form__textarea" />
-            <button type="submit" className="contact-form__button">Submit</button>
+            <textarea placeholder="Como podriamos ayudarte?" maxLength="120" className="contact-form__textarea" />
+            <button type="submit" className="contact-form__button">Enviar</button>
             <p className="contact-form__footer">
-              By contacting us, you agree to our <a href="#" className="contact-form__link">Terms of service</a> and <a href="#" className="contact-form__link">Privacy Policy</a>
             </p>
           </form>
         </div>
--- src/components/Contactanos/ContactUs.jsx
+++ src/components/Contactanos/ContactUs.jsx
@@ -18,15 +18,55 @@ const ContactUs = ({ nombre, desc, precio, img }) => {
               ¡Esperamos saber de ti pronto!
             </p>
             <br />
-            <p>
-                email@gmail.com
-            </p>
+            <p>email@gmail.com</p>
             <br />
-            <p>
-                1124613507
-            </p>
+            <p>1124613507</p>
+            <div className="seccionesCajas">
+              <div className="caja">
+                <h4>Atención al Cliente</h4>
+                <p>
+                  Nuestro equipo está disponible para atender cualquier consulta
+                  o duda que puedas tener sobre el buffet.
+                </p>
+              </div>
+              <div className="caja">
+                <h4>Sugerencias y Comentarios</h4>
+                <p>
+                  Valoramos tus opiniones y estamos constantemente mejorando el
+                  buffet. Tu feedback es crucial para ofrecer un mejor servicio.
+                </p>
+              </div>
+              <div className="caja">
+                <h4>Consultas Generales</h4>
+                <p>
+                  Para cualquier consulta general o información adicional, por
+                  favor contáctanos en: buffet@escuela36.edu.ar
+                </p>
+              </div>
+            </div>
           </div>
           <ContactForm />
+        </div>
+      </div>
+      <div className="pre2">
+        <iframe
+          id="mapa"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.7208997259436!2d-58.496517024416704!3d-34.56062137296977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb6f1ce409681%3A0xed70920e983fcb6a!2sEscuela%20T%C3%A9cnica%2036%20D.E.%2015%20-%20Alte.%20Brown!5e0!3m2!1ses!2sar!4v1724206646230!5m2!1ses!2sar"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />{" "}
+        <div className="contenido">
+          <h2>Nuestra Ubicación</h2>
+          <h3>Sabores que nos Unen</h3>
+          <h4>Buffet Escolar</h4>
+          <p>Escuela Técnica 36 D.E. 15 - Alte. Brown</p>
+          <p>Galván 3700, C1431FVK</p>
+          <p>Ciudad Autónoma de Buenos Aires</p>
+          <p>Argentina</p>
         </div>
       </div>
     </div>
